e538f9bc3af235908d5d4903fcb3dd663bb1212a
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-12 11:12:54 -0300

    Questao 2.15

---
+++ chapter2/Aritmetica.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Aritmetica {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first integer:");
+        float n1 = input.nextFloat();
+
+        System.out.print("Enter the second integer:");
+        float n2 = input.nextFloat();
+
+        System.out.printf("The sum of %.2f and %.2f is %.2f%n", n1, n2, n1 + n2);
+        System.out.printf("The subtraction of %.2f and %.2f is %.2f%n", n1, n2, n1 - n2);
+        System.out.printf("The product of %.2f and %.2f is %.2f%n", n1, n2, n1 * n2);
+        System.out.printf("The division of %.2f and %.2f is %.2f%n", n1, n2, n1 / n2);
+    }
+}
