528a5012e4801c182409ee0afdc757b5d4f6cb4b
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-14 14:48:50 -0300

    Novo programa de Par ou Impar

---
+++ chapter2/ParOuImpar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Enter a number:");
+        int n = input.nextInt();
+
+        if (n % 2 == 0)
+            System.out.println("The number " + n +" is even");
+        else
+            System.out.println("The number " + n + " is odd");
+    }
+}
