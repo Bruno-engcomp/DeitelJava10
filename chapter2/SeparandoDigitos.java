93defa565ab95e66ef05569e369952adfd63be8b
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-15 15:20:26 -0300

    Atualizacao

--- chapter2/SeparandoDigitos.java
+++ chapter2/SeparandoDigitos.java
@@ -7,7 +7,12 @@ public class SeparandoDigitos {
         System.out.print("Enter a number with 5 digits:");
         float n = input.nextFloat();
 
-        System.out.printf("%.0f %.0f %.0f %.0f %.0f", n/10000, (n%10000) / 1111, (n%1000) / 111 , (n%100) / 11 , n%10);
+        if(n > 99999)
+            System.out.println("Error, the number is bigger then 99999");
+        else if (n < 10000)
+            System.out.println("Error, the number is smaller then 10000");
+        else
+            System.out.printf("%.0f %.0f %.0f %.0f %.0f", (n/10000)/1.1, (n%10000) / 1111, (n%1000) / 111 , (n%100) / 11 , n%10);
 
     }
 }
