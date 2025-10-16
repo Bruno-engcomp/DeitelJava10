8c3c6baea7f811d8daeb5493b38b05bee0f555f2
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-16 14:47:31 -0300

    Novo programa de imc

---
+++ chapter2/IMC.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IMC {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your weight:");
+        float w = input.nextFloat();
+
+        System.out.print("Enter your height:");
+        float h = input.nextFloat();
+        float imc = w /(h*h);
+
+        if  (imc < 18.5)
+            System.out.println("Underweight: less than 18.5");
+        else if (imc < 25)
+            System.out.println("Normal: between 18.5 and 24.9");
+        else if (imc < 30)
+            System.out.println("Overweight: between 25 and 29.9");
+        else
+            System.out.println("Obese: 30 or greater");
+    }
+}
