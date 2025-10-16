9160916cc478b51e710b17c80b74e1f47e71d773
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-16 14:32:12 -0300

    Novo programa

---
+++ chapter2/TabelaDeQuadradosECubos.java
@@ -0,0 +1,16 @@
+public class TabelaDeQuadradosECubos {
+    public static void main(String[] args) {
+        System.out.println("\tNumber\tSquare\tCube");
+        System.out.println("\t 1    \t 1    \t 1");
+        System.out.println("\t 2    \t 4    \t 8");
+        System.out.println("\t 3    \t 9    \t 27");
+        System.out.println("\t 4    \t 16   \t 64");
+        System.out.println("\t 5    \t 25   \t 125");
+        System.out.println("\t 6    \t 36   \t 216");
+        System.out.println("\t 7    \t 49   \t 343");
+        System.out.println("\t 8    \t 64   \t 512");
+        System.out.println("\t 9    \t 81   \t 729");
+        System.out.println("\t 10   \t100   \t 1000");
+
+    }
+}
