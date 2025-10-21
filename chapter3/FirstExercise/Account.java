8f368a2fab3716a04b403121cd32ff1f2308b52d
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-20 23:05:18 -0300

    Account file

---
+++ chapter3/FirstExercise/Account.java
@@ -0,0 +1,40 @@
+package FirstExercise;
+
+public class Account {
+    private String name; // variável de instância
+    private double balance; // variável de instância
+
+    public Account(String name, double balance)
+    {
+        this.name = name; // atribui name à variável de instância name
+
+        // valida que o balance é maior que 0.0; se não for,
+        // a variável de instância balance mantém seu valor inicial padrão de 0.0
+        if(balance > 0.0) // se o saldo for válido
+            this.balance = balance; // o atribui à variável de instância balance
+    }
+    // método que deposita (adiciona) apenas uma quantia válida no saldo
+    public void deposit(double depositAmount)
+    {
+        if (depositAmount > 0.0) //Se o deposito for valido
+            balance = balance + depositAmount; // o adiciona ao saldo
+
+    }
+
+    // Metodo que retorna o saldo de conta
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    // Metodo que define o nome
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    // Metodo para retorna o nome
+    public String getName()
+    {
+        return name; // retorna o valor de name ao chamador
+    } // Fim do metodo getName
+} // fim da classe Account
