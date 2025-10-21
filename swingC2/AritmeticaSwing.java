a469467089c2fe77b62dc2a07dcb67a2ddbaff0b
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-20 22:51:58 -0300

    Correcao

--- swingC2/AritmeticaSwing.java
+++ swingC2/AritmeticaSwing.java
@@ -8,6 +8,6 @@ public class AritmeticaSwing {
         int n1 = Integer.parseInt(aux1);
         int n2 = Integer.parseInt(aux2);
 
-        JOptionPane.showMessageDialog(null, "The sum of " + n1 + " and " + n2 + " is " + (n1 + n2) + "\nThe subtract of " + n1 + " and " + n2 + " is " + (n1 - n2) + "\nThe product of " + n1 + " and " + n2 + " is " + (n1 * n2));\
+        JOptionPane.showMessageDialog(null, "The sum of " + n1 + " and " + n2 + " is " + (n1 + n2) + "\nThe subtract of " + n1 + " and " + n2 + " is " + (n1 - n2) + "\nThe product of " + n1 + " and " + n2 + " is " + (n1 * n2));
     }
 }
