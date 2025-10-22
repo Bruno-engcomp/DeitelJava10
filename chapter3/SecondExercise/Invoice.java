0dd1ca731d1db830c6688062dd5937f588d89187
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-21 21:00:45 -0300

    Starting Invoice class

--- chapter3/SecondExercise/Invoice.java
+++ chapter3/SecondExercise/Invoice.java
@@ -1,4 +1,31 @@
 package SecondExercise;
 
 public class Invoice {
+    String name;
+    String description;
+    int number;
+    double price;
+
+    public void getInvoiceAmount()
+
+
+    // Metodo que define o nome do produto
+    public void setName(String name) {this.name = name;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public void setNumber(int number) {this.number = number;}
+
+    public void setPrice(double price) {this.price = price;}
+
+    public String getName() { return name; }
+
+    public String getDescription() { return description; }
+
+    public int getNumber() { return number; }
+
+    public double getPrice() { return price; }
+
+
+
 }
