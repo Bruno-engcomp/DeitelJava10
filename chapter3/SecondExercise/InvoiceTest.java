99f645bda283081cec86d629563c5710e74dba58
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-23 18:23:38 -0300

    Variaveis para mudanca de nome e descricao

--- chapter3/SecondExercise/InvoiceTest.java
+++ chapter3/SecondExercise/InvoiceTest.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class InvoiceTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String nname;
+        String ndescription;
 
         Invoice keyboard = new Invoice("keyboard","The best keyboard", 25.00);
         Invoice mouse = new Invoice("mouse", "A really nice mouse", 10.00);
@@ -20,5 +22,7 @@ public class InvoiceTest {
         System.out.printf("Enter the new price of keyboard: ");
         nprice = input.nextDouble();
         keyboard.setPrice(nprice);
+
+        System.out.println("The new price of keyboard is " + keyboard.getPrice());
     }
 }
