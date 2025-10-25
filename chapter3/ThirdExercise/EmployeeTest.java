32f02959ae1ebfcd141f02145e8b100f0f972075
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-25 15:22:49 -0300

    Start of EmployeeTest

--- chapter3/ThirdExercise/EmployeeTest.java
+++ chapter3/ThirdExercise/EmployeeTest.java
@@ -1,4 +1,18 @@
 package ThirdExercise;
 
+import java.util.Scanner;
+
 public class EmployeeTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+        String lastName;
+        String Desc;
+        double salary;
+
+        Employee Employee1 = new Employee("Allana","Silva","The best worker of the company", 100000.00);
+        Employee Employee2 = new Employee("Bruno", "Almeida", "Is not better than Allana, but is very good too", 10000.00);
+
+        
+    }
 }
