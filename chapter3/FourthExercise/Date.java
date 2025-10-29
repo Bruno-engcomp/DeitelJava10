68fae97726dcdeac5775ed837b0ac747a541cd80
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-29 19:41:38 -0300

    Classe date completa

--- chapter3/FourthExercise/Date.java
+++ chapter3/FourthExercise/Date.java
@@ -1,7 +1,36 @@
 package FourthExercise;
 
 public class Date {
-    private int dd;
-    private int aa;
-    private int mm;
+    private int day;
+    private int year;
+    private int month;
+
+
+    void displayDate ()
+    {
+        System.out.printf("The date is %d/%d/%d%n",getMonth(),getDay(),getYear());
+    }
+
+    int getDay () {return day;}
+    int getMonth () {return month;}
+    int getYear () {return year;}
+
+    void setDay (int day)
+    {
+        if(day < 32 && day > 0)
+            this.day = day;
+    }
+    void setMonth (int month)
+    {
+        if (month > 0 && month < 13)
+            this.month = month;
+    }
+    void setYear (int year)
+    {
+        if(year > 0)
+            this.year = year;
+    }
+
+
+
 }
