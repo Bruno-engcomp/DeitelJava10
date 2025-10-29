3c882dc3d5977a95b6885cf70fe83c8c0d6ed88e
bruno.almd <brunoalmeida22007@gmail.com>
2025-10-29 19:58:39 -0300

    DateTest finished

--- chapter3/FourthExercise/DisplayDate.java
+++ chapter3/FourthExercise/DisplayDate.java
@@ -1,4 +1,27 @@
 package FourthExercise;
 
+import java.util.Scanner;
 public class DisplayDate {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int day;
+        int month;
+        int year;
+
+        Date date1 = new Date (29,10,2025);
+        date1.displayDate ();
+        System.out.print("Enter the day:");
+        day = input.nextInt();
+        date1.setDay(day);
+
+        System.out.print("Enter the month:");
+        month = input.nextInt();
+        date1.setMonth(month);
+
+        System.out.print("Enter the year:");
+        year = input.nextInt();
+        date1.setYear(year);
+
+        date1.displayDate ();
+    }
 }
