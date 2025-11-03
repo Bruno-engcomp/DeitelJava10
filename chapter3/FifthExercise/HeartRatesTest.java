67f27c7e20e417bb1e9b912211cfe0a4faeb5f90
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-03 18:17:33 -0300

    Teste das funcoes set

--- chapter3/FifthExercise/HeartRatesTest.java
+++ chapter3/FifthExercise/HeartRatesTest.java
@@ -1,11 +1,25 @@
 package FifthExercise;
+import java.util.Scanner;
 
 public class HeartRatesTest {
     public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
         HeartRates Patient1 = new HeartRates("Bruno", "Almeida", 6,9,2007);
         HeartRates Patient2 = new HeartRates("Allana", "Kamilla", 14,3,2007);
         displayInformations(Patient1);
         displayInformations(Patient2);
+
+        System.out.print("Enter the name of Patient1: ");
+        Patient1.setName(input.nextLine());
+
+        System.out.print("Enter the surname of Patient1: ");
+        Patient1.setSurname(input.nextLine());
+
+        System.out.print("Enter the new birthday (just the day) of Patient1: ");
+        Patient1.setBirthday(input.nextInt());
+
+        displayInformations(Patient1);
 
     }
 
