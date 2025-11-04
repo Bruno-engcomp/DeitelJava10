526469f8b50848c55bfb60ee0dfc7422bacd3acf
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-04 07:14:23 -0300

    Definicao de atributos

--- chapter3/SixthExercise/HealthProfile.java
+++ chapter3/SixthExercise/HealthProfile.java
@@ -1,4 +1,14 @@
 package SixthExercise;
 
 public class HealthProfile {
+    String name;
+    String surname;
+    String sex;
+    int dd;
+    int mm;
+    int yy;
+    float height;
+    float weight;
+
+    
 }
