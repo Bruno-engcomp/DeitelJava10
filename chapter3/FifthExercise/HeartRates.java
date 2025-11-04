6e6325ce6214903b33b169ea2df6f3f2084ccd88
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-04 07:08:49 -0300

    Programa finalizado

--- chapter3/FifthExercise/HeartRates.java
+++ chapter3/FifthExercise/HeartRates.java
@@ -23,7 +23,12 @@ public class HeartRates {
     {
         System.out.printf("%d/%d/%d", getBirthday(), getBirthmonth(), getBirthyear());
     }
-
+    void heartRateInterval(int birthyear)
+    {
+        double maxHeartRate = 220 - (2025 - birthyear);
+        System.out.printf("The max Heart Rate is %.1f%n", maxHeartRate);
+        System.out.printf("The tarjet heart rate is from %f to %.1f%n%n", maxHeartRate * 0.5, maxHeartRate * 0.85);
+    }
     // Funcoes para armazenar nas variaveis
     public void setName (String name) {this.name = name;}
     public void setSurname (String surname) {this.surname = surname;}
--- chapter3/FifthExercise/HeartRatesTest.java
+++ chapter3/FifthExercise/HeartRatesTest.java
@@ -8,7 +8,9 @@ public class HeartRatesTest {
         HeartRates Patient1 = new HeartRates("Bruno", "Almeida", 6,9,2007);
         HeartRates Patient2 = new HeartRates("Allana", "Kamilla", 14,3,2007);
         displayInformations(Patient1);
+        Patient1.heartRateInterval(Patient1.getBirthyear());
         displayInformations(Patient2);
+        Patient2.heartRateInterval(Patient2.getBirthyear());
 
         System.out.print("Enter the name of Patient1: ");
         Patient1.setName(input.nextLine());
@@ -22,6 +24,7 @@ public class HeartRatesTest {
         displayInformations(Patient1);
         System.out.printf("The age o patient1 is: %d", Patient1.getAge());
         Patient1.getAge();
+
     }
 
     public static void displayInformations(HeartRates patient)
