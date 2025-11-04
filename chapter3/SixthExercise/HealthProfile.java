de9eae17e94b549c48080619aabef8bc4c0b7329
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-04 07:22:56 -0300

    Metodo Healt profile, validacao dos atributos

--- chapter3/SixthExercise/HealthProfile.java
+++ chapter3/SixthExercise/HealthProfile.java
@@ -3,12 +3,31 @@ package SixthExercise;
 public class HealthProfile {
     String name;
     String surname;
-    String sex;
+    String sex; // Male or Female
     int dd;
     int mm;
     int yy;
-    float height;
-    float weight;
+    float height; // In meters
+    float weight; // In Kilograms
 
-    
+    public HealthProfile (String name, String surname, String sex, int dd, int mm, int yy, float height, float weight)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.sex = sex;
+
+        if (dd > 0 && dd < 32)
+            this.dd = dd;
+        if (mm > 0 && mm < 13)
+            this.mm = mm;
+        if (yy > 0 && < 2026)
+            this.yy = yy;
+        if (height > 0.20)
+            this.height = height;
+        if (weight > 2.6)
+            this.weight = weight;
+
+    }
+
+
 }
