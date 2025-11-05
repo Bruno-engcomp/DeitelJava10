1ab1f6e301715a421bb3efbdaf0595728cd52397
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-05 10:49:41 -0300

    Metodos de teste funcionando

--- chapter3/SixthExercise/HealthProfileTest.java
+++ chapter3/SixthExercise/HealthProfileTest.java
@@ -5,19 +5,26 @@ public class HealthProfileTest {
     public static void main(String[] args) {
         HealthProfile patient1 = new HealthProfile("Bruno", "Almeida", "Male", 6, 9, 2007, 1.83 , 75.00);
         HealthProfile patient2 = new HealthProfile("Allana", "Kamilla", "Female", 14, 3, 2007, 1.64, 50.00);
+
+        DisplayHealthProfile(patient1);
+        heartRateInterval(patient1);
+        DisplayHealthProfile(patient2);
+        heartRateInterval(patient2);
+
     }
 
     public static void DisplayHealthProfile(HealthProfile Patient)
     {
-        System.out.printf("Name: %s%nSurname: %s%n", Patient.getName(),Patient.getSurname());
+        System.out.printf("%nName: %s%nSurname: %s%n", Patient.getName(),Patient.getSurname());
         System.out.printf("Birthday %d/%d/%d%n", Patient.getDd(),Patient.getMm(),Patient.getYy());
-        System.out.printf("Age: %d", Patient.getAge());
+        System.out.printf("Age: %d%n", Patient.getAge());
+        System.out.printf("IMC: %.2f%n", Patient.getImc());
     }
 
     public static void heartRateInterval(HealthProfile Patient)
     {
         double maxHeartRate = 220 - (2025 - Patient.getYy());
         System.out.printf("The max Heart Rate is %.1f%n", maxHeartRate);
-        System.out.printf("The tarjet heart rate is from %f to %.1f%n%n", maxHeartRate * 0.5, maxHeartRate * 0.85);
+        System.out.printf("The tarjet heart rate is from %.1f to %.1f%n", maxHeartRate * 0.5, maxHeartRate * 0.85);
     }
 }
