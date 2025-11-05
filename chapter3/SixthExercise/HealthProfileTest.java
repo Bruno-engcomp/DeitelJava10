a4fbb4059825d04fef74b0cc9a252a9a3f5e9e2d
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-05 10:58:46 -0300

    Adição de sexo no metodo DisplayHealthProfile e criação do método setMetods

--- chapter3/SixthExercise/HealthProfileTest.java
+++ chapter3/SixthExercise/HealthProfileTest.java
@@ -1,5 +1,5 @@
 package SixthExercise;
-
+import java.util.Scanner;
 
 public class HealthProfileTest {
     public static void main(String[] args) {
@@ -16,6 +16,7 @@ public class HealthProfileTest {
     public static void DisplayHealthProfile(HealthProfile Patient)
     {
         System.out.printf("%nName: %s%nSurname: %s%n", Patient.getName(),Patient.getSurname());
+        System.out.printf("Sex: %s%n", Patient.getSex());
         System.out.printf("Birthday %d/%d/%d%n", Patient.getDd(),Patient.getMm(),Patient.getYy());
         System.out.printf("Age: %d%n", Patient.getAge());
         System.out.printf("IMC: %.2f%n", Patient.getImc());
@@ -26,5 +27,28 @@ public class HealthProfileTest {
         double maxHeartRate = 220 - (2025 - Patient.getYy());
         System.out.printf("The max Heart Rate is %.1f%n", maxHeartRate);
         System.out.printf("The tarjet heart rate is from %.1f to %.1f%n", maxHeartRate * 0.5, maxHeartRate * 0.85);
+    }
+
+    public static void setMetods(HealthProfile Patient)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the name: ");
+        Patient.setName(input.nextLine());
+
+        System.out.print("Enter the surname: ");
+        Patient.setSurname(input.nextLine());
+
+        System.out.print("Enter the sex: ");
+        Patient.setSex(input.nextLine());
+
+        System.out.print("Enter the birthday: ");
+        Patient.setDd(input.nextInt());
+
+        System.out.print("Enter the birthmonth: ");
+        Patient.setMm(input.nextInt());
+
+        System.out.print("Enter the birthyear: ");
+        Patient.setYy(input.nextInt());
     }
 }
