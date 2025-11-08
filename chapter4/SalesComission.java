ffebc6a025391257feaf206fe7685d5e2d2cfb69
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-08 20:09:09 -0300

    SalesComission Finished

--- chapter4/SalesComission.java
+++ chapter4/SalesComission.java
@@ -1,5 +1,22 @@
-package SecondExercise;
-
+import java.util.Scanner;
 public class SalesComission {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double totalSales = 0.0;
+        double sold = 0.0;
+
+        System.out.println("If you want to stop enter a number less than 0");
+        while(true)
+        {
+            System.out.print("Enter the price of the sold item:");
+            sold = input.nextDouble();
+            if(sold < 0)
+                break;
+            totalSales += sold;
+
+
+        }
+        System.out.printf("The seller's income was: %.2f", 200 + (totalSales * 0.09));
+
+    }
 }
