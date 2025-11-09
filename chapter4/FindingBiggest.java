694bc69b6d1e1f0049c79330beda4a8c5e286e67
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-08 21:30:38 -0300

    Biggest Number program

---
+++ chapter4/FindingBiggest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class FindingBiggest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bNumber = 0;
+        int n;
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.print("Enter a number: ");
+            n = input.nextInt();
+            if (n > bNumber)
+                bNumber = n;
+        }
+        System.out.printf("The biggest number is %d", bNumber);
+    }
+}
