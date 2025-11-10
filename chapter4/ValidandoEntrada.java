c9a14de32e592b2bf24a9d35eb9aa022d6b6a0f9
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-10 18:46:28 -0300

    ValidandoEntrada Finished

--- chapter4/ValidandoEntrada.java
+++ chapter4/ValidandoEntrada.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class ValidandoEntrada {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = 0;
+        while (n != 1 && n != 2)
+        {
+            System.out.print("Insira o numero valido: ");
+            n = input.nextInt();;
+        }
+        System.out.print("Parabéns você inseriu o número certo");
+    }
 }
