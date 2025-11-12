8f2793e5879cc525ed60128ce055e97af6ee707e
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-12 16:47:23 -0300

    Tabuleiro de Damas finalizado

--- chapter4/TabuleiroDamas.java
+++ chapter4/TabuleiroDamas.java
@@ -1,2 +1,20 @@
 public class TabuleiroDamas {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++)
+        {
+            if(i % 2 == 0) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print("* ");
+                }
+            }
+            else
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
