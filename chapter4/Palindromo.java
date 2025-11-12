b27c5d41d8342f2627630da832a79d928df39341
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-12 13:43:21 -0300

    Inversao de numeros

--- chapter4/Palindromo.java
+++ chapter4/Palindromo.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Palindromo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, original, invertido, resto;
+        while(true)
+        {
+            System.out.print("Enter a five digit number: ");
+            n = input.nextInt();
+            if(n > 9999 && n < 100000)
+                break;
+            System.out.println("Error, the digited number is not a five digit number");
+        }
+        original = n;
+        invertido = 0;
+
+        while(n > 0)
+        {
+            resto = n % 10;
+            invertido = invertido * 10 + resto;
+            n /= 10;
+        }
+        System.out.print(invertido);
+
+
+    }
 }
