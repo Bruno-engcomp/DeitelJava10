818a6457b06525247f67ada499f00a27d8c43de8
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-14 18:25:54 -0300

    New program

--- chapter4/Exponencial.java
+++ chapter4/Exponencial.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Exponencial {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number:");
+        int n = input.nextInt();
+
+        double e, result = 1;
+        double f = 1;
+
+        for (int i = 1; i < 10; i++)
+        {
+            e = Math.pow(n, i);
+            f *= i;
+            result += e/f;
+        }
+        System.out.printf("%f", result);
 
     }
 }
