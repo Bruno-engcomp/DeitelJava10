3d35659d4c620368ae2b10d3fb17c3115a696c40
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-17 12:00:11 -0300

    Soma de 1 a 100

--- chapter4/Criptography.java
+++ chapter4/Criptography.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Criptography {
     public static void main(String[] args) {
+        int n, n1, n2, n3 ,n4, aux, encrypted;
+        while(true)
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the four digit number: ");
+            n = input.nextInt();
 
+            if(n > 999 & n < 10000 )
+            {
+                break;
+            }
+            System.out.println("Enter a valid number");
+        }
+        n1 = n / 1000;
+        n2 = (n / 100) % 10;
+        n3 = (n / 10) % 10;
+        n4 = n % 10 ;
+
+        n1 = (n1 + 7) % 10;
+        n2 = (n2 + 7) % 10;
+        n3 = (n3 + 7) % 10;
+        n4 = (n4 + 7) % 10;
+
+        // Troca do 1 pelo 3
+        aux = n1;
+        n1 = n3;
+        n3 = aux;
+
+        // Troca do 2 pelo 4
+        aux = n2;
+        n2 = n4;
+        n4 = aux;
+
+        encrypted = (n1 * 1000) + (n2 * 100) + (n3 * 10) + n4;
+        System.out.printf(" %d will be %d", n, encrypted);
     }
 }
--- chapter5/Soma1a100.java
+++ chapter5/Soma1a100.java
@@ -1,2 +1,10 @@
 public class Soma1a100 {
+    public static void main(String[] args) {
+        int n = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+           n += i;
+        }
+        System.out.println("Soma de 1 a 100 = " + n);
+    }
 }
