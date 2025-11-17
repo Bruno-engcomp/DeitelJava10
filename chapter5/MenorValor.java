ca394df2adccc47fdfd77a305a42de8c6d22b421
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-17 12:07:19 -0300

    New program

---
+++ chapter5/MenorValor.java
@@ -0,0 +1,7 @@
+
+
+public class MenorValor {
+    public static void main(String[] args) {
+
+    }
+}
--- chapter5/Soma1a100.java
+++ chapter5/Soma1a100.java
@@ -3,8 +3,10 @@ public class Soma1a100 {
         int n = 0;
         for (int i = 1; i <= 100; i++)
         {
-           n += i;
+            if(i % 2 != 0)
+                n += i;
         }
         System.out.println("Soma de 1 a 100 = " + n);
+        System.out.println(Math.pow(2.5, 3));
     }
 }
