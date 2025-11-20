1ff14acec7281a49500b67fe2788698d2ad48344
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-20 14:13:03 -0300

    Diamond with a variable size side

--- chapter5/LosanguloNlado.java
+++ chapter5/LosanguloNlado.java
@@ -1,10 +1,15 @@
-
+import java.util.Scanner;
 
 public class LosanguloNlado {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++)
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o tamanho do lado do losangulo: ");
+        int n = input.nextInt();
+        n--;
+
+        for(int i = 0; i < n; i++)
         {
-            for (int j = 5; j > i; j--) {
+            for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
             for (int j = 0; j < (i*2)+1; j++) {
@@ -12,9 +17,9 @@ public class LosanguloNlado {
             }
             System.out.println();
         }
-        for(int i = 5; i >= 0; i--)
+        for(int i = n; i >= 0; i--)
         {
-            for (int j = 5; j > i; j--) {
+            for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
             for (int j = 0; j < (i*2)+1; j++) {
