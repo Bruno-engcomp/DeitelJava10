495fae15cd02b1e3b30d19d3b07562dbc80be9c1
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-20 15:09:41 -0300

    Musica de natal

---
+++ chapter5/TheTwelveDaysOfChristmas.java
@@ -0,0 +1,176 @@
+import java.util.Scanner;
+
+public class TheTwelveDaysOfChristmas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 1; i <= 12; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("Firts day");
+                    break;
+                case 2:
+                    System.out.println("Second day");
+                    break;
+                case 3:
+                    System.out.println("Third day");
+                    break;
+                case 4:
+                    System.out.println("Fourth day");
+                    break;
+                case 5:
+                    System.out.println("Fifth day");
+                    break;
+                case 6:
+                    System.out.println("Sixth");
+                    break;
+                case 7:
+                    System.out.println("Seventh");
+                    break;
+                case 8:
+                    System.out.println("Eighth");
+                    break;
+                case 9:
+                    System.out.println("Nineth");
+                    break;
+                case 10:
+                    System.out.println("Tenth");
+                    break;
+                case 11:
+                    System.out.println("Eleventh");
+                    break;
+                case 12:
+                    System.out.println("Twelfth");
+                    break;
+            }
+            switch (i) {
+                case 1:
+                    System.out.println("On the first day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "A partridge in a pear tree. ");
+                    break;
+                case 2:
+                    System.out.println("On the second day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 3:
+                    System.out.println("On the third day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 4:
+                    System.out.println("On the fourth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 5:
+                    System.out.println("On the fifth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 6:
+                    System.out.println("On the sixth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 7:
+                    System.out.println("On the seventh day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Seven swans a swimming,\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree.\n" +
+                            "\n");
+                    break;
+                case 8:
+                    System.out.println("On the eighth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Eight maids a milking,\n" +
+                            "Seven swans a swimming,\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 9:
+                    System.out.println("On the ninth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Nine ladies dancing,\n" +
+                            "Eight maids a milking,\n" +
+                            "Seven swans a swimming,\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 10:
+                    System.out.println("On the tenth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Ten lords a leaping,\n" +
+                            "Nine ladies dancing,\n" +
+                            "Eight maids a milking,\n" +
+                            "Seven swans a swimming,\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 11:
+                    System.out.println("On the eleventh day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Eleven pipers piping,\n" +
+                            "Ten lords a leaping,\n" +
+                            "Nine ladies dancing,\n" +
+                            "Eight maids a milking,\n" +
+                            "Seven swans a swimming,\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree. ");
+                    break;
+                case 12:
+                    System.out.println("On the twelfth day of Christmas\n" +
+                            "My true love sent to me:\n" +
+                            "Twelve drummers drumming,\n" +
+                            "Eleven pipers piping,\n" +
+                            "Ten lords a leaping,\n" +
+                            "Nine ladies dancing,\n" +
+                            "Eight maids a milking,\n" +
+                            "Seven swans a swimming,\n" +
+                            "Six geese a laying,\n" +
+                            "Five golden rings,\n" +
+                            "Four calling birds,\n" +
+                            "Three French Hens,\n" +
+                            "Two turtle doves\n" +
+                            "And a Partridge in a pear tree.");
+                    break;
+            }
+        }
+    }
+}
