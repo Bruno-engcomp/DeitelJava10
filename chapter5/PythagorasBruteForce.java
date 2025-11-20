bfa79902dfd78393a7019a682f2dc90e56f0cc03
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-20 13:34:04 -0300

    Programa que procura triangulos pitagoricos com o lado maximo de 500

--- chapter5/PythagorasBruteForce.java
+++ chapter5/PythagorasBruteForce.java
@@ -1,2 +1,15 @@
 public class PythagorasBruteForce {
+    public static void main(String[] args) {
+        for (int i = 1; i < 500; i++) {
+            for (int j = 1; j < 500; j++) {
+                for (int k = 1; k < 500; k++) {
+                    if((i*i)+(j*j)==(k*k))
+                        System.out.printf("%d + %d = %d is a Pythagorean triangle%n",i ,j, k);
+
+                }
+
+            }
+
+        }
+    }
 }
--- chapter5/ValorPI.java
+++ chapter5/ValorPI.java
@@ -1,2 +1,16 @@
 public class ValorPI {
+    public static void main(String[] args) {
+        double pi = 4;
+        int j = 2;
+
+        for (long i = 3; i < 10000000L; i += 2)
+        {
+            if(j % 2 == 0)
+                pi -= (4.0 / i);
+            else
+                pi += (4.0 / i);
+            j++;
+        }
+        System.out.printf("%f", pi);
+    }
 }
